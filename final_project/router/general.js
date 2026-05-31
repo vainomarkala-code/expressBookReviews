d11e118daeb65833ad85bce444b61520920edc65
vainomarkala-code <vaino.markala@gmail.com>
2026-05-31 14:51:09 -0400

    update and test

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: JSON.stringify(books,null,4)});
  });
   
 // Get book details based on author
