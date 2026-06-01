281d58e3ed372b23e0458f4be37db4dc80503342
vainomarkala-code <vaino.markala@gmail.com>
2026-06-01 03:13:54 -0400

    kafa

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
 
 
 public_users.post("/register", (req, res) => {
@@ -35,10 +36,13 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    //Write your code here
-    res.send(JSON.stringify(books, null, 4));
-    return res.status(300).json({ message: "Yet to be implemented" });
+public_users.get('/', async function (req, res) {
+    try {
+        const response = await axios.get("http://localhost:5001/");
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books" });
+    }
 });
 
 // Get book details based on ISBN
